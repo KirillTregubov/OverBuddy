33545784ba9fc07a783433f273811a7a28fc7531
Kirill Tregubov <contact@kirilltregubov.com>
2024-01-11 13:32:39 -0500

    formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -13,16 +13,24 @@ export default defineConfig(async () => ({
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
+      ignored: ['**/src-tauri/**']
+    }
   },
-  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  envPrefix: [
+    'VITE_',
+    'TAURI_PLATFORM',
+    'TAURI_ARCH',
+    'TAURI_FAMILY',
+    'TAURI_PLATFORM_VERSION',
+    'TAURI_PLATFORM_TYPE',
+    'TAURI_DEBUG'
+  ],
   build: {
     // // Tauri uses Chromium on Windows and WebKit on macOS and Linux
     target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     // // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' as const : false,
+    minify: !process.env.TAURI_DEBUG ? ('esbuild' as const) : false,
     // // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  },
-}));
+    sourcemap: !!process.env.TAURI_DEBUG
+  }
+}))
