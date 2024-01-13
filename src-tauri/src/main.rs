f82f137c7336b241ca5c09a4d2c155944c74c14a
Kirill Tregubov <contact@kirilltregubov.com>
2024-01-13 05:12:57 -0500

    work on writing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Serialize;
+use serde_json::json;
+use serde_json::Serializer;
+use std::fs;
+use std::io::prelude::*;
+
 // create the error type that represents all errors possible in our program
 #[derive(Debug, thiserror::Error)]
 enum Error {
@@ -40,26 +46,28 @@ fn display_path_string(path: &std::path::PathBuf) -> Result<String, Error> {
         })
 }
 
-fn fetch_config(handle: tauri::AppHandle) -> Result<(bool, String), Error> {
+fn fetch_config(handle: tauri::AppHandle) -> Result<String, Error> {
     let app_data_dir = handle.path_resolver().app_data_dir().unwrap();
     let resource_path = app_data_dir.join("../Battle.net");
     let target_file_name = "Battle.net.config";
 
     // Check if the target file exists in the directory
-    if let Ok(entries) = std::fs::read_dir(&resource_path) {
+    if let Ok(entries) = fs::read_dir(&resource_path) {
         if let Some(target_entry) = entries
             .filter_map(|entry| entry.ok()) // Filter out errors
             .find(|entry| entry.file_name().to_string_lossy() == target_file_name)
         {
             let display_path = display_path_string(&target_entry.path())?;
-            let result = format!("Found configuration: {}", display_path);
-            // println!("{}", result);
-            return Ok((true, result));
+            // println!("{}", display_path);
+            return Ok(display_path);
         } else {
             let display_path = display_path_string(&resource_path)?;
-            let result = format!("File {} not found in {}", target_file_name, display_path);
+            let result = format!(
+                "Battle.net AppData ({}) doesn't contain file \"{}\"",
+                display_path, target_file_name
+            );
             // println!("{}", result);
-            return Ok((false, result));
+            return Err(Error::Custom(result));
         }
     } else {
         let result = format!("Failed to read Battle.net AppData directory.");
@@ -68,35 +76,96 @@ fn fetch_config(handle: tauri::AppHandle) -> Result<(bool, String), Error> {
     }
 }
 
-// use serde_json::json;
-
-#[tauri::command]
-fn set_map(map: &str) -> Result<String, Error> {
-    return Ok(format!("Loading {}!", map));
-}
-
-#[derive(serde::Serialize)]
-struct JsonResponse {
-    success: bool,
-    result: String,
-}
+// #[derive(serde::Serialize)]
+// struct JsonResponse {
+//     success: bool,
+//     result: String,
+// }
 
 #[tauri::command]
 fn get_config(handle: tauri::AppHandle) -> Result<String, Error> {
-    let (success, result) = fetch_config(handle)?;
+    let config = fetch_config(handle)?;
+    return Ok(config);
+}
 
-    let response = JsonResponse {
-        success: success,
-        result,
+#[tauri::command]
+fn set_background(handle: tauri::AppHandle, name: &str) -> Result<String, Error> {
+    let config = fetch_config(handle)?;
+
+    println!("Loaded {}", config);
+
+    let mut file = fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(config.clone())?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut json: serde_json::Value = serde_json::from_str(&contents)?;
+
+    println!("before: {}", json);
+
+    if json
+        .get("Games")
+        .and_then(|games| games.get("prometheus"))
+        .is_none()
+    {
+        return Err(Error::Custom(format!(
+            "Failed to find Overwatch in your Battle.net config ({})",
+            config
+        )));
+    }
+
+    let launch_args = json["Games"]["prometheus"]["AdditionalLaunchArguments"]
+        .as_str()
+        .map(|s| s.to_string());
+
+    let new_arg = format!("--lobbyMap={}", "0x0800000000000864");
+    let launch_args: String = match launch_args {
+        Some(arguments) => {
+            let filtered_args = arguments
+                .split_whitespace()
+                .filter(|&part| !part.starts_with("--lobbyMap"))
+                .collect::<Vec<&str>>()
+                .join(" ");
+
+            if !filtered_args.is_empty() {
+                format!("{} {}", new_arg, filtered_args)
+            } else {
+                new_arg
+            }
+        }
+        None => new_arg,
     };
-    let json_response = serde_json::to_string(&response)?;
 
-    return Ok(json_response);
+    json["Games"]["prometheus"]["AdditionalLaunchArguments"] = json!(launch_args);
+    // format!(
+    //     "--lobbyMap={}{}",
+    //     "0x0800000000000864", launch_args
+    // ));
+
+    let pretty_formatter = serde_json::ser::PrettyFormatter::with_indent(b"    ");
+    let mut serializer = Serializer::with_formatter(&mut file, pretty_formatter);
+    json.serialize(&mut serializer)?;
+
+    println!("\"{}\"", launch_args);
+
+    // let launch_args = json["Games"]["prometheus"]["AdditionalLaunchArguments"]
+    //     .as_str()
+    //     .unwrap()
+    //     .to_string();
+
+    // println!("Background: {}", launch_args);
+
+    println!("\nafter: {}", json);
+
+    return Ok(format!("Setting {}!", name));
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![set_map, get_config])
+        .invoke_handler(tauri::generate_handler![get_config, set_background])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,56 @@
 import { useState } from 'react'
 import { invoke } from '@tauri-apps/api/tauri'
-import { exists, BaseDirectory, readDir } from '@tauri-apps/api/fs'
 
 function App() {
+  const [configPath, setConfigPath] = useState('')
+  const [error, setError] = useState('')
   const [response, setResponse] = useState('')
-  const [data, setData] = useState({} as any)
 
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  //   setGreetMsg(await invoke('greet', { name }))
-  // }
+  async function get_config() {
+    setConfigPath('')
+    setError('')
+    try {
+      setConfigPath(await invoke('get_config'))
+    } catch (error) {
+      setError(error as string)
+    }
+  }
+
+  async function set_background() {
+    setResponse('')
+    setError('')
+    try {
+      setResponse(await invoke('set_background', { name: 'test' }))
+    } catch (error) {
+      setError(error as string)
+    }
+  }
 
   return (
-    <div className="p-4">
-      <button
-        className="select-none rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white transition-[color,background-color,border-color,text-decoration-color,fill,stroke,transform] will-change-transform hover:bg-neutral-700 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-neutral-300 active:scale-95"
-        onClick={async () => {
-          setResponse('')
-          try {
-            const val = await invoke('get_config')
-            console.log(val)
-            setData(JSON.parse(val as string))
-          } catch (error) {
-            setResponse(error as string)
-          }
-        }}
-      >
-        Greet
-      </button>
-      {/* <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault()
-          greet()
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
+    <div className="flex flex-col gap-4 p-4">
+      <div>
+        <button
+          className="select-none rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white transition-[color,background-color,border-color,text-decoration-color,fill,stroke,transform] will-change-transform hover:bg-neutral-700 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-neutral-300 active:scale-95"
+          onClick={get_config}
+        >
+          Get Config
+        </button>
+        <div>{configPath}</div>
+      </div>
 
-      */}
-      <code>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </code>
-      <p>{response}</p>
+      <div>
+        <button
+          className="select-none rounded-lg bg-black px-5 py-2.5 text-sm font-medium text-white transition-[color,background-color,border-color,text-decoration-color,fill,stroke,transform] will-change-transform hover:bg-neutral-700 focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-neutral-300 active:scale-95"
+          onClick={set_background}
+        >
+          Set Background
+        </button>
+        <div>{response}</div>
+      </div>
+
+      {error.length > 0 && (
+        <p className="font-bold text-red-500">Error: {error}</p>
+      )}
     </div>
   )
 }
