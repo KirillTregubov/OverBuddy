99c2e562dcffaf518bd1b70a06ea225d589ed527
Kirill Tregubov <contact@kirilltregubov.com>
2024-01-15 13:56:32 -0500

    formatting and animations

--- src/routes/menu.tsx
+++ src/routes/menu.tsx
@@ -1,19 +1,21 @@
 import { FileRoute, redirect } from '@tanstack/react-router'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { useRef, useState } from 'react'
+import { motion } from 'framer-motion'
 import clsx from 'clsx'
 import {
   ChevronLeft,
-  ChevronRight,
-  HeartIcon,
-  SettingsIcon
+  ChevronRight
+  // HeartIcon,
+  // SettingsIcon
 } from 'lucide-react'
+
 import {
   useBackgroundsMutation,
   backgroundsQueryOptions,
   launchQueryOptions
 } from '../data'
 import placeholder from '../assets/placeholder.svg'
-import { useSuspenseQuery } from '@tanstack/react-query'
 
 export const Route = new FileRoute('/menu').createRoute({
   loader: ({ context: { queryClient } }) =>
@@ -80,7 +82,12 @@ function Menu() {
   }
 
   return (
-    <div className="relative flex h-full w-full flex-col gap-4 p-6">
+    <motion.div
+      className="relative flex h-full w-full flex-col gap-4 p-6"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
       {/* <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">
           Main Menu <span className="font-medium">Customizer</span>
@@ -95,7 +102,7 @@ function Menu() {
       <div className="relative -mt-2">
         <div className="scrollbar-hide flex h-40 flex-shrink-0 items-center gap-3 overflow-x-auto scroll-smooth px-12">
           {data.map((background, index) => (
-            <img
+            <motion.img
               id={background.id}
               key={background.id}
               alt={background.name}
@@ -103,29 +110,43 @@ function Menu() {
                 'aspect-video cursor-pointer select-none snap-center rounded-lg object-cover shadow-orange-600/15 drop-shadow-md transition-[width,height,border-radius,box-shadow,transform] will-change-transform',
                 activeBackground.id === background.id
                   ? 'h-36 w-64 rounded-xl shadow-md'
-                  : 'h-28 w-52 shadow-sm hover:scale-105'
+                  : 'h-28 w-52 shadow-sm hover:!scale-105'
               )}
               src={`/backgrounds/${background.image}`}
               ref={(el) => (backgroundRefs.current[index] = el!)}
               onClick={() => handleSelect(index)}
               onError={onImageError}
+              initial={{ transform: 'scale(.9)' }}
+              animate={{ transform: 'scale(1)' }}
+              transition={{ duration: 0.3 }}
             />
           ))}
         </div>
-        <div
+        <motion.div
           className="absolute left-1 top-1/2 -translate-y-1/2 transform cursor-pointer rounded-full bg-zinc-800/90 p-1 mix-blend-luminosity"
           onClick={() => handleNavigate('prev')}
+          initial={{ transform: 'translateX(15px)' }}
+          animate={{ transform: 'translateX(0)' }}
+          transition={{ duration: 0.3 }}
         >
           <ChevronLeft size={24} className="text-white" />
-        </div>
-        <div
+        </motion.div>
+        <motion.div
           className="absolute right-1 top-1/2 -translate-y-1/2 transform cursor-pointer  rounded-full bg-zinc-800/90 p-1 mix-blend-luminosity"
           onClick={() => handleNavigate('next')}
+          initial={{ transform: 'translateX(-15px)' }}
+          animate={{ transform: 'translateX(0)' }}
+          transition={{ duration: 0.3 }}
         >
           <ChevronRight size={24} className="text-zinc-100" />
-        </div>
+        </motion.div>
       </div>
-      <div className="relative flex h-full min-h-0 w-full flex-1 justify-center">
+      <motion.div
+        className="relative flex h-full min-h-0 w-full flex-1 justify-center"
+        initial={{ transform: 'scale(.95)' }}
+        animate={{ transform: 'scale(1)' }}
+        transition={{ duration: 0.3 }}
+      >
         <img
           alt="Selected Wallpaper"
           className="h-full w-[61rem] select-none rounded-lg object-cover" //w-[55rem]
@@ -133,14 +154,14 @@ function Menu() {
           onError={onImageError}
         />
         <div className="absolute bottom-0 flex w-full items-center gap-4 bg-gradient-to-b from-transparent to-zinc-950/50 to-25% p-4 pt-8">
-          <button className="group ml-auto rounded-full border-2 border-orange-900 bg-orange-950 p-3.5 text-orange-100 shadow-md transition-[border-color,transform,fill] will-change-transform hover:scale-105 hover:border-white focus-visible:scale-105 focus-visible:border-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white active:scale-95">
+          {/* <button className="group ml-auto rounded-full border-2 border-orange-900/50 bg-orange-950 p-3.5 text-orange-100 shadow-md ring-white transition-[border-color,transform,fill] will-change-transform hover:scale-105 hover:border-white focus-visible:scale-105 focus-visible:border-white focus-visible:outline-none focus-visible:ring-2 active:scale-95 active:border-orange-200 active:ring-orange-200">
             <HeartIcon
               size={24}
               className="fill-transparent transition-colors group-hover:fill-current group-focus-visible:fill-current group-active:fill-orange-200 group-active:stroke-orange-200"
             />
-          </button>
+          </button> */}
           <button
-            className="select-none rounded-[0.2rem] border-2 border-orange-800/40 bg-orange-500 px-10 py-3 text-lg font-medium uppercase tracking-wider text-white shadow-md transition-[border-color,transform,border-radius] will-change-transform hover:scale-105 hover:rounded-[0.25rem] hover:border-white focus-visible:scale-105 focus-visible:border-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white active:scale-95"
+            className="ml-auto select-none rounded-[0.2rem] border-2 border-orange-800/40 bg-orange-500 px-10 py-3 text-lg font-medium uppercase tracking-wider text-white shadow-md transition-[border-color,transform,border-radius] will-change-transform hover:scale-105 hover:rounded-[0.25rem] hover:border-white focus-visible:scale-105 focus-visible:border-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white active:scale-95"
             onClick={() => mutate({ id: activeBackground.id })}
           >
             {mutationStatus === 'pending' ? (
@@ -152,7 +173,7 @@ function Menu() {
             )}
           </button>
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
