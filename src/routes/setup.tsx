1174ff03fbb9a440dd4cdf96024b0a13181b3ed1
Kirill Tregubov <contact@kirilltregubov.com>
2024-01-14 21:17:00 -0500

    backup

--- src/routes/setup.tsx
+++ src/routes/setup.tsx
@@ -1,6 +1,6 @@
 import { FileRoute, Outlet } from '@tanstack/react-router'
 
-export const Route = new FileRoute('/setup').createRoute({
+export const Route = new FileRoute('/setup/_layout').createRoute({
   component: Setup
 })
 
