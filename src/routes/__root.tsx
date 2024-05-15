075e1e2f15bc70df09787a0748344271d8ae2419
Kirill Tregubov <contact@kirilltregubov.com>
2024-05-15 06:35:05 -0400

    Release 1.1.2

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -33,7 +33,7 @@ export const Route = createRootRouteWithContext<{
       throw redirect({ to: '/menu' })
     }
   },
-  errorComponent: RootErrorComponent as any,
+  errorComponent: RootErrorComponent,
   component: RootComponent
 })
 
